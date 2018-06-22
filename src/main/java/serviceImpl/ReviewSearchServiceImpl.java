b90378041b6b39f969200cf484894851a63f32f8
ObserverZQ <657282348@qq.com>
2018-06-22 15:12:52 +0800

    天杀的带逗号helpfulness（再见（太菜了叭

--- src/main/java/serviceImpl/ReviewSearchServiceImpl.java
+++ src/main/java/serviceImpl/ReviewSearchServiceImpl.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import service.ReviewSearchService;
 import wordcount.WordUtil;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,7 +21,7 @@ import java.util.Map;
  * Created by mac on 2017/5/12.
  */
 @Service("reviewSearchService")
-public class ReviewSearchServiceImpl implements ReviewSearchService{
+public class ReviewSearchServiceImpl implements ReviewSearchService {
 
     @Autowired
     private ReviewDataDao reviewDataDao;
@@ -38,7 +41,7 @@ public class ReviewSearchServiceImpl implements ReviewSearchService{
 
     @Override
     public List<Review> getReviews(String imdb_filmID) {
-        if(buf_reviewList == null||!buf_filmID.equals(imdb_filmID)) {
+        if (buf_reviewList == null || !buf_filmID.equals(imdb_filmID)) {
             buf_reviewList = reviewDataDao.getReviews(imdb_filmID);
             buf_filmID = imdb_filmID;
         }
@@ -47,7 +50,7 @@ public class ReviewSearchServiceImpl implements ReviewSearchService{
 
     @Override
     public List<Review> getReviews(String imdb_filmID, int n) {
-        if(buf_reviewList == null||!buf_filmID.equals(imdb_filmID)) {
+        if (buf_reviewList == null || !buf_filmID.equals(imdb_filmID)) {
             buf_reviewList = reviewDataDao.getReviews(imdb_filmID, n);
             buf_filmID = imdb_filmID;
         }
@@ -56,26 +59,26 @@ public class ReviewSearchServiceImpl implements ReviewSearchService{
 
     @Override
     public List<Review> getReviews(String imdb_filmID, ReviewSort reviewSort) {
-        if(!buf_filmID.equals(imdb_filmID)){
+        if (!buf_filmID.equals(imdb_filmID)) {
             buf_reviewList = reviewDataDao.getReviews(imdb_filmID);
             buf_filmID = imdb_filmID;
         }
 
-        if(reviewSort.equals(ReviewSort.MostHelpful)) {
+        if (reviewSort.equals(ReviewSort.MostHelpful)) {
             //按helpfulness 从大到小排序
             Collections.sort(buf_reviewList, (o1, o2) -> {
                 int[] a1 = new int[2];
                 int[] a2 = new int[2];
-                for(int i=0;i<2;i++) {
+                for (int i = 0; i < 2; i++) {
                     a1[i] = Integer.parseInt(o1.getHelpfulness().split("/")[i]);
                     a2[i] = Integer.parseInt(o2.getHelpfulness().split("/")[i]);
                 }
                 Double a1_ = 0.0;
                 Double a2_ = 0.0;
-                if(a1[1] != 0)
-                    a1_ = ((double)a1[0]) / a1[1];
-                if(a2[1] != 0)
-                    a2_ = ((double)a2[0]) / a2[1];
+                if (a1[1] != 0)
+                    a1_ = ((double) a1[0]) / a1[1];
+                if (a2[1] != 0)
+                    a2_ = ((double) a2[0]) / a2[1];
 
                 //权重计算
                 a1_ = a1_ * VALUE_WEIGHT + a1[1] * NUM_WEIGHT;
@@ -83,12 +86,12 @@ public class ReviewSearchServiceImpl implements ReviewSearchService{
 
                 return a2_.compareTo(a1_);
             });
-        }else{
+        } else {
             Collections.sort(buf_reviewList, (o1, o2) -> {
                 LocalDate a = o1.getTime();
                 LocalDate b = o2.getTime();
 
-                if(reviewSort.equals(ReviewSort.Oldest))
+                if (reviewSort.equals(ReviewSort.Oldest))
                     return a.compareTo(b);
                 else
                     return b.compareTo(a);
@@ -101,26 +104,56 @@ public class ReviewSearchServiceImpl implements ReviewSearchService{
 
     @Override
     public List<Review> getReviews(String imdb_filmID, ReviewSort reviewSort, int n) {
-        if(!buf_filmID.equals(imdb_filmID)){
+        if (!buf_filmID.equals(imdb_filmID)) {
             buf_reviewList = reviewDataDao.getReviews(imdb_filmID, n);
             buf_filmID = imdb_filmID;
         }
 
-        if(reviewSort.equals(ReviewSort.MostHelpful) || reviewSort.equals(ReviewSort.LeastHelpful)) {
+        if (reviewSort.equals(ReviewSort.MostHelpful) || reviewSort.equals(ReviewSort.LeastHelpful)) {
             //按helpfulness 从大到小排序
             Collections.sort(buf_reviewList, (o1, o2) -> {
                 int[] a1 = new int[2];
                 int[] a2 = new int[2];
-                for(int i=0;i<2;i++) {
-                    a1[i] = Integer.parseInt(o1.getHelpfulness().split("/")[i]);
-                    a2[i] = Integer.parseInt(o2.getHelpfulness().split("/")[i]);
+                a1[0] = a1[1] = a2[0] = a2[1] = 0;
+                String temp1 = "";
+                String temp11 = "";
+                String temp2 = "";
+                String temp22 = "";
+                if (o1.getHelpfulness().length()>2) {
+                    temp1 = o1.getHelpfulness().split("/")[0];
+                    temp11 = o1.getHelpfulness().split("/")[1];
                 }
+                if (o2.getHelpfulness().length()>2) {
+                    temp2 = o2.getHelpfulness().split("/")[0];
+                    temp22 = o2.getHelpfulness().split("/")[1];
+                }
+                try {
+                    if (isNumeric(temp1)) {
+                        a1[0] = new DecimalFormat().parse(temp1).intValue();
+                    }
+                    if (isNumeric(temp11)) {
+                        a1[1] = new DecimalFormat().parse(temp11).intValue();
+                    }
+                    if (isNumeric(temp2)) {
+                        a2[0] = new DecimalFormat().parse(temp2).intValue();
+                    }
+                    if (isNumeric(temp22)) {
+                        a2[1] = new DecimalFormat().parse(temp22).intValue();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(a1[0] + " / " + a1[1]);
+                System.out.println(a2[0] + " / " + a2[1]);
                 Double a1_ = 0.0;
                 Double a2_ = 0.0;
-                if(a1[1] != 0)
-                    a1_ = ((double)a1[0]) / a1[1];
-                if(a2[1] != 0)
-                    a2_ = ((double)a2[0]) / a2[1];
+                if (a1[1] != 0) {
+                    a1_ = ((double) a1[0]) / a1[1];
+                }
+                if (a2[1] != 0) {
+                    a2_ = ((double) a2[0]) / a2[1];
+                }
 
                 //权重计算
                 a1_ = a1_ * VALUE_WEIGHT + a1[1] * NUM_WEIGHT;
@@ -131,12 +164,12 @@ public class ReviewSearchServiceImpl implements ReviewSearchService{
                     return a1_.compareTo(a2_);
                 }
             });
-        }else{
+        } else {
             Collections.sort(buf_reviewList, (o1, o2) -> {
                 LocalDate a = o1.getTime();
                 LocalDate b = o2.getTime();
 
-                if(reviewSort.equals(ReviewSort.Oldest))
+                if (reviewSort.equals(ReviewSort.Oldest))
                     return a.compareTo(b);
                 else
                     return b.compareTo(a);
@@ -149,26 +182,26 @@ public class ReviewSearchServiceImpl implements ReviewSearchService{
 
     @Override
     public Map<String, Integer> getReviewsKeyWords(String imdb_filmID, int n) {
-        if(!buf_filmID.equals(imdb_filmID)){
+        if (!buf_filmID.equals(imdb_filmID)) {
             buf_reviewList = reviewDataDao.getReviews(imdb_filmID);
-            buf_keywords = WordUtil.keyWordSearch(buf_reviewList,n);
+            buf_keywords = WordUtil.keyWordSearch(buf_reviewList, n);
             buf_filmID = imdb_filmID;
         }
-        if(buf_keywords == null)
-            buf_keywords = WordUtil.keyWordSearch(buf_reviewList,n);
+        if (buf_keywords == null)
+            buf_keywords = WordUtil.keyWordSearch(buf_reviewList, n);
 
         return buf_keywords;
     }
 
     @Override
     public List<Review> filterReviewsByKeyWord(String imdb_filmID, String keyword) {
-        if(!buf_filmID .equals(imdb_filmID)){
+        if (!buf_filmID.equals(imdb_filmID)) {
             buf_reviewList = reviewDataDao.getReviews(imdb_filmID);
             buf_filmID = imdb_filmID;
         }
         List<Review> result = new ArrayList<>();
-        for(Review review : buf_reviewList){
-            if(review.getText().contains(keyword))
+        for (Review review : buf_reviewList) {
+            if (review.getText().contains(keyword))
                 result.add(review);
         }
 
@@ -178,5 +211,19 @@ public class ReviewSearchServiceImpl implements ReviewSearchService{
     @Override
     public long getReviewNumber(String imdb_filmID) {
         return reviewDataDao.getReviewNumber(imdb_filmID);
+    }
+
+    public boolean isNumeric(String str) {
+        try {
+            new DecimalFormat().parse(str).intValue();
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("异常：\"" + str + "\"不是数字/整数...");
+            return false;
+        }
+        catch (ParseException e) {
+            System.out.println("转换异常：\"" + str + "\"不是数字/整数...");
+            return false;
+        }
     }
 }
